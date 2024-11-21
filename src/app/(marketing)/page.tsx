6fc86708b1220aebd48adef97a582199a82e2132
Simplyalex99 <alexmflores99@hotmail.ca>
2024-11-21 17:05:27 -0500

    add: home page

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+import { Hero, Testimonials, Pricing } from './_components'
+import { Footer } from '@/components'
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Testimonials />
+      <Pricing />
+      <Footer />
+    </>
+  )
+}
+export default Home
