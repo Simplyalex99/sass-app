72b09c5eae0b1b226ce861e75783f5990f3010c0
Simplyalex99 <alexmflores99@hotmail.ca>
2024-11-21 17:02:59 -0500

    add: UI components

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+export { Button } from './ui/button'
+export { BrandLogo } from './ui/brandLogo'
+export {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardDescription,
+  CardTitle,
+} from './ui/card'
+export { Footer } from './ui/footer'
