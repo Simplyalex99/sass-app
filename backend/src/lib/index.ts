39d4c3916cf9eadb8c335efc30a73d2f1a8f1afd
Simplyalex99 <alexmflores99@hotmail.ca>
2025-01-14 12:16:14 -0500

    add(backend): libs and class helpers

---
+++ backend/src/lib/index.ts
@@ -0,0 +1,4 @@
+export { redisClient, connectRedis } from './config/redis'
+export { UserSchema, UserSchemaType } from './schemas/userSchema'
+export { userService } from './services/userService'
+export { AppError } from './errors/app'
---
+++ backend/src/lib/schemas/userSchema.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod'
+import { isPasswordValid } from '#utils'
+export const UserSchema = z.object({
+  email: z.string({ message: 'Missing required field email' }).email(),
+  plainTextPassword: z
+    .string({ message: 'Missing required field password' })
+    .refine(isPasswordValid, {
+      message:
+        'Password does not meet one or more of the required criterias: Password must have at least 8 characters, max 64 characters, 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character',
+    }),
+})
+export type UserSchemaType = z.infer<typeof UserSchema>
