3db4a751b6e1ad6ce43f0d390956b47cbab624fc
Simplyalex99 <alexmflores99@hotmail.ca>
2025-01-14 12:15:14 -0500

    add(backend): constants

---
+++ backend/src/enums/index.ts
@@ -0,0 +1,13 @@
+export {
+  ACCESS_TOKEN_KEY,
+  REFRESH_TOKEN_KEY,
+  AUTHORIZATION_KEY,
+  CSRF_TOKEN_KEY,
+} from './auth'
+export {
+  TierNames,
+  PaidTierNames,
+  subscriptionTiersInOrder,
+  subscriptionTiers,
+} from './tierNames'
+export { INTERNAL_SERVER_ERROR } from './errorStatusCode'
