474a41d5f3875dd9354e87022449c442e57f2512
Simplyalex99 <alexmflores99@hotmail.ca>
2025-02-02 13:30:57 -0500

    update(api): moved service files into separate folder

--- api/src/lib/index.ts
+++ api/src/lib/index.ts
@@ -4,8 +4,9 @@ export {
   RegisterUserSchemaType,
 } from './schemas/registerUserSchema'
 export { LoginUserSchema, LoginUserSchemaType } from './schemas/loginUserSchema'
+export { SendEmailSchema, SendEmailSchemaType } from './schemas/sendEmailSchema'
 export { userService } from './services/db/userService'
 export { subscriptionService } from './services/db/subscriptionService'
 export { AppError } from './errors/app'
-export { EmailService } from './services/emailService'
+export { EmailService } from './services/others/emailService'
 export { createEmailVerificationHtml } from './others/createEmailVerificationHtml'
