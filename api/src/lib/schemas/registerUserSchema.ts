5c9c7957ffb732be6b1a61c5287e4ab859e1b2b3
Simplyalex99 <alexmflores99@hotmail.ca>
2025-02-02 13:27:14 -0500

    update(api): added error message to register schema and created send email schema

--- api/src/lib/schemas/registerUserSchema.ts
+++ api/src/lib/schemas/registerUserSchema.ts
@@ -7,17 +7,23 @@ export const RegisterUserSchema = z.object({
       message: 'Invalid email address',
     }),
   passwordForm: z
-    .object({
-      plainTextPassword: z
-        .string({ message: 'Missing required field password' })
-        .refine(isPasswordValid, {
-          message:
-            'Password does not meet one or more of the required criterias: Password must have at least 8 characters, max 64 characters, 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character',
+    .object(
+      {
+        plainTextPassword: z
+          .string({ message: 'Missing required field password' })
+          .refine(isPasswordValid, {
+            message:
+              'Password does not meet one or more of the required criterias: Password must have at least 8 characters, max 64 characters, 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character',
+          }),
+        confirmPassword: z.string({
+          message: 'Missing required field confirm password',
         }),
-      confirmPassword: z.string({
-        message: 'Missing required field confirm password',
-      }),
-    })
+      },
+      {
+        message:
+          'Missing required fields email, and passwordForm with fields plainTextPassword and confirmPassword',
+      }
+    )
     .refine(
       (data) => {
         return data.confirmPassword === data.plainTextPassword
