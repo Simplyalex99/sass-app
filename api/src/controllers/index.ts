adb5c88e322637383166284a3293ecfe564bb8a4
Simplyalex99 <alexmflores99@hotmail.ca>
2025-02-08 10:37:42 -0500

    update(api): added login controller exports

--- api/src/controllers/index.ts
+++ api/src/controllers/index.ts
@@ -1,3 +1,4 @@
 export { createUserByEmailController } from './createUserByEmail'
 export { sendEmailValidationController } from './sendEmailVerfication'
 export { verifyEmailController } from './verifyEmail'
+export { loginController } from './login'
