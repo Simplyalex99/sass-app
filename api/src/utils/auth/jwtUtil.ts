47205ea4ae837545af0fb7a8b9fa588c419129cd
Simplyalex99 <alexmflores99@hotmail.ca>
2025-02-08 10:35:06 -0500

    refactor(api): moved error messages into variables

--- api/src/utils/auth/jwtUtil.ts
+++ api/src/utils/auth/jwtUtil.ts
@@ -6,7 +6,10 @@ import {
 } from '#enums'
 const JWT_ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_TOKEN_SECRET
 const JWT_REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_TOKEN_SECRET
-
+const refreshTokenErrorMessage =
+  'JWT_REFRESH_TOKEN_SECRET environment variable not found.'
+const accessTokenErrorMessage =
+  'JWT_ACCESS_TOKEN_SECRET environment variable not found.'
 export class JWTUtil {
   private constructor() {
     if (this.constructor == JWTUtil) {
@@ -16,7 +19,7 @@ export class JWTUtil {
 
   static createAccessToken(data: string | Buffer | object) {
     if (!JWT_ACCESS_TOKEN_SECRET) {
-      throw new Error('JWT_ACCESS_TOKEN_SECRET environment variable not found.')
+      throw new Error(accessTokenErrorMessage)
     }
     const accessToken = jwt.sign(data, JWT_ACCESS_TOKEN_SECRET, {
       expiresIn: ACCESS_TOKEN_EXPIRY_DATE_IN_SECONDS,
@@ -25,13 +28,26 @@ export class JWTUtil {
   }
   static createRefreshToken(data: string | Buffer | object) {
     if (!JWT_REFRESH_TOKEN_SECRET) {
-      throw new Error(
-        'JWT_REFRESH_TOKEN_SECRET environment variable not found.'
-      )
+      throw new Error(refreshTokenErrorMessage)
     }
     const refreshToken = jwt.sign(data, JWT_REFRESH_TOKEN_SECRET, {
       expiresIn: REFRESH_TOKEN_EXPIRY_DATE_IN_SECONDS,
     })
     return refreshToken
+  }
+  static verifyAccessToken(accessToken: string) {
+    if (!JWT_ACCESS_TOKEN_SECRET) {
+      throw new Error(accessTokenErrorMessage)
+    }
+    const result = jwt.verify(accessToken, JWT_ACCESS_TOKEN_SECRET)
+    return result
+  }
+
+  static verifyRefreshToken(refreshToken: string) {
+    if (!JWT_REFRESH_TOKEN_SECRET) {
+      throw new Error(refreshTokenErrorMessage)
+    }
+    const result = jwt.verify(refreshToken, JWT_REFRESH_TOKEN_SECRET)
+    return result
   }
 }
