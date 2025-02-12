1ede3439c728b324d8e6a8155cdd430c63cd42c1
Simplyalex99 <alexmflores99@hotmail.ca>
2025-02-12 10:51:33 -0500

    add(api): logger middleware

---
+++ api/src/middlewares/loggerMiddleware.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from 'express'
+import { log } from '../utils/others/log'
+import { AppError } from '../lib/errors/app'
+export const loggerMiddleware = (
+  err: AppError | Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  log.debug('made it here')
+  if (err instanceof Error && err.stack) {
+    log.error('%O', err)
+  }
+  next()
+}
