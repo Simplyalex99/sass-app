8978c48f7b920c87b910a9ba15019155d2eb116c
Simplyalex99 <alexmflores99@hotmail.ca>
2025-02-12 10:53:08 -0500

    fix(api): custom error middleware not working due to missing next param. Also removed logs.

--- api/src/middlewares/errorMiddleware.ts
+++ api/src/middlewares/errorMiddleware.ts
@@ -1,15 +1,16 @@
-import { Request, Response } from 'express'
-import { AppError } from '#lib'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response } from 'express'
+import { AppError } from '../lib/errors/app'
+import { INTERNAL_SERVER_ERROR } from '#enums'
 export const errorMiddleware = (
   err: AppError | Error,
   req: Request,
-  res: Response
+  res: Response,
+  next: NextFunction
 ) => {
-  if (err instanceof AppError) {
-    res.status(err.statusCode).send()
+  if (err instanceof Error && err.stack !== undefined) {
+    res.status(500).json({ error: INTERNAL_SERVER_ERROR })
   }
-  if (err.stack) {
-    res.status(500).send()
-  }
+
   res.status(404).json({ error: 'Page not found' })
 }
