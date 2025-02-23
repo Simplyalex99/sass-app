2ffec3394b3abd62b83bd48ceb3e614722fd8c96
Simplyalex99 <alexmflores99@hotmail.ca>
2025-02-23 18:28:51 -0500

    update(frontend): added API types

---
+++ frontend/src/types/api.ts
@@ -0,0 +1,17 @@
+export type RegisterUserBody = {
+  error?: string
+}
+export type SendEmailBody = {
+  error?: string
+  message: string | null
+}
+export type VerifyEmailBody = {
+  error?: string
+}
+export type LoginBody = {
+  error?: string
+  user?: {
+    isEmailVerified: boolean
+    data?: { userId: string; withOauthLogin: boolean }
+  }
+}
