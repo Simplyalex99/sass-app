582ff7c6933c048a675892f23653145d5e5e9a1d
Simplyalex99 <alexmflores99@hotmail.ca>
2025-03-04 17:27:27 -0500

    add: password validation for client/server side

---
+++ frontend/src/utils/helpers/validatePassword.ts
@@ -0,0 +1,41 @@
+const MIN_PASSWORD_LENGTH = 8
+const MAX_PASSWORD_LENGTH = 64
+
+const hasSpecialCharacter = (str: string) => {
+  const specialCharRegex = /[@.#$!%*?&]/
+  return specialCharRegex.test(str)
+}
+const hasNumber = (str: string) => {
+  const specialCharRegex = /\d/
+  return specialCharRegex.test(str)
+}
+const hasUppercase = (str: string) => {
+  const specialCharRegex = /[A-Z]/
+  return specialCharRegex.test(str)
+}
+const hasLowercase = (str: string) => {
+  const specialCharRegex = /[a-z]/
+  return specialCharRegex.test(str)
+}
+export const validatePassword = (plainTextPassword: string): string[] => {
+  const messages: Array<string> = []
+  if (plainTextPassword.length < MIN_PASSWORD_LENGTH) {
+    messages.push('At least 8 characters')
+  }
+  if (plainTextPassword.length > MAX_PASSWORD_LENGTH) {
+    messages.push('Max 64 characters')
+  }
+  if (!hasNumber(plainTextPassword)) {
+    messages.push('At least 1 number (0-9)')
+  }
+  if (!hasUppercase(plainTextPassword)) {
+    messages.push('At least 1 uppercase')
+  }
+  if (!hasLowercase(plainTextPassword)) {
+    messages.push('At least 1 lowercase')
+  }
+  if (!hasSpecialCharacter(plainTextPassword)) {
+    messages.push('At least 1 special character @.#$!%*?&')
+  }
+  return messages
+}
