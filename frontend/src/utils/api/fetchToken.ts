fdac77082615bfedc970bdb23196e71f81bb7988
Simplyalex99 <alexmflores99@hotmail.ca>
2025-03-05 10:31:51 -0500

    update: switched to using variable links

--- frontend/src/utils/api/fetchToken.ts
+++ frontend/src/utils/api/fetchToken.ts
@@ -1,14 +1,15 @@
 import { fetchData } from '../others/fetchData'
 import { RequestOTPBody } from '@/types/api'
+import { requestOtpApi, resetPasswordApi } from '@/constants/api'
 export const fetchToken = async (id: string | null) => {
-  const result = await fetchData<RequestOTPBody>('/api/email/request', {
+  const result = await fetchData<RequestOTPBody>(requestOtpApi, {
     method: 'POST',
     body: JSON.stringify({ userId: id }),
   })
   return result.body
 }
 export const requestPasswordReset = async (email: string) => {
-  const result = await fetchData<RequestOTPBody>('/api/auth/password/reset', {
+  const result = await fetchData<RequestOTPBody>(resetPasswordApi, {
     method: 'POST',
     body: JSON.stringify({ email }),
   })
