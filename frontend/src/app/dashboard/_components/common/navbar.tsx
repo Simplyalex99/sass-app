476e14e176cbab2fe9c2b4fe70cd713380f482fb
Simplyalex99 <alexmflores99@hotmail.ca>
2025-03-12 17:21:50 -0400

    update: added dashboard and sign-out links to each navbars respectively

--- frontend/src/app/dashboard/_components/common/navbar.tsx
+++ frontend/src/app/dashboard/_components/common/navbar.tsx
@@ -1,6 +1,26 @@
+'use client'
 import Link from 'next/link'
-import { BrandLogo } from '@/components'
+import { BrandLogo, Button } from '@/components'
+import { fetchSignOut } from '@/utils/api/api'
+import { signOut } from 'next-auth/react'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { fetchUser } from '@/utils/reactQuery/api'
+import { userKey } from '@/constants/reactQuery'
 export const NavBar = () => {
+  const queryClient = useQueryClient()
+  const { mutateAsync } = useMutation({
+    mutationFn: fetchUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [userKey] })
+    },
+  })
+
+  const signOutHandler = async () => {
+    signOut()
+    await fetchSignOut()
+    await mutateAsync()
+  }
+
   return (
     <header className="flex bg-background py-4 shadow">
       <nav className="container flex items-center gap-10">
@@ -10,6 +30,9 @@ export const NavBar = () => {
         <Link href="/dashboard/products">Products</Link>
         <Link href="/dashboard/analytics">Analytics</Link>
         <Link href="/dashboard/subscription">Subscription</Link>
+        <Button onClick={signOutHandler} className="text-lg">
+          Sign out
+        </Button>
       </nav>
     </header>
   )
